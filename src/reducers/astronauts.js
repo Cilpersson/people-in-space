08199555161af874f3eec80fe7d6e825d9e25a10
Christina Persson <christina@MacBook-Pro.local>
2020-05-03 21:11:22 +0200

    tested cors fetch

--- src/reducers/astronauts.js
+++ src/reducers/astronauts.js
@@ -16,11 +16,27 @@ export const astronauts = createSlice({
 export const onStart = () => {
   return (dispatch) => {
     dispatch(ui.actions.setLoading(true));
-    fetch(`http://api.open-notify.org/astros.json`)
+    // fetch(`https://www.howmanypeopleareinspacerightnow.com/peopleinspace.json`)
+    //   .then((data) => data.json())
+    //   .then((json) => {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url =
+      "https://www.howmanypeopleareinspacerightnow.com/peopleinspace.json"; // site that doesn’t send Access-Control-*
+    fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
       .then((data) => data.json())
       .then((json) => {
         dispatch(astronauts.actions.setAstronauts(json));
         dispatch(ui.actions.setLoading(false));
-      });
+      })
+      .catch(() =>
+        console.log("Can’t access " + url + " response. Blocked by browser?")
+      );
   };
 };
+
+// const proxyurl = "https://cors-anywhere.herokuapp.com/";
+// const url =
+//   "https://www.howmanypeopleareinspacerightnow.com/peopleinspace.json"; // site that doesn’t send Access-Control-*
+// fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+//   .then((response) => response.text())
+//   .then((contents) => console.log(contents))
